b112066036c94b73e2f8e520af846d1f48f4b4ef
IsmailShpati <75791043+IsmailShpati@users.noreply.github.com>
2021-01-23 21:48:25 +0100

    ..

--- Tech-Store Managment'/src/views/CashierView.java
+++ Tech-Store Managment'/src/views/CashierView.java
@@ -10,21 +10,41 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import models.SideMenu;
 import view_models.AddItem;
 import view_models.ItemsView;
 
 public class CashierView  implements Viewable{
 	
+
 	private BorderPane root = new BorderPane();
 	private ItemsView leftSide = new ItemsView();
 	private AddItem rightSide = new AddItem(leftSide);
+    private SideMenu menu = new SideMenu();
 	
 	public CashierView() {
 		root.setLeft(leftSide);
 		leftSide.setAddItemView(rightSide);
 		root.setRight(rightSide);
 		initBottom();
+		initSideMenu();
+	}
+	
+	/*
+	 * public CashierView(Cashier cashier){
+	 * 		this.cashier = cashier;
+	 * }
+	 */
+	
+	private void initSideMenu() {
+		menu.addButton(root, "Scan items");
+		StackPane pn = new StackPane();
+		pn.getChildren().add(new Circle(100, Color.BLACK));
+		menu.addButton(pn, "Prov");
 	}
 	
 	
@@ -50,8 +70,9 @@ public class CashierView  implements Viewable{
 	@Override
 	public void setView(Stage stage) {
 
+		stage.sizeToScene();
 		stage.setTitle("Cashier view");
-		stage.setScene(new Scene(root));
+		stage.setScene(new Scene(menu));
 		
 	}
 }
